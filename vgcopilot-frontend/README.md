bd355fb90bbd4fdb12db6788dafac919d2a55b6e
David Alejandro Moreno Zapata <33850883+dam322@users.noreply.github.com>
2023-05-20 17:08:49 -0500

    Delete vgcopilot-frontend directory

--- vgcopilot-frontend/README.md
+++
@@ -1,1 +0,0 @@
-# vgcopilot-frontend
--- vgcopilot-frontend/src/recommendationsSharedState.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil';
-import { Game } from './rawgTypes';
-export const recommendationsReqBoby = atom<{results: Game[], user_input: {name: string, rating_user: number}[]}>({
-  key: 'reqBoby',
-  default: undefined,
-})
-
-
